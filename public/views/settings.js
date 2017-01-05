806891e05d2b0260f5df2691d8f66aaee1bae742
BrainFooLong <roland@bfldev.com>
2017-01-05 17:12:12 +0100

    updates for logging

--- public/views/settings.js
+++ public/views/settings.js
@@ -1,9 +1,62 @@
 "use strict";
 View.register("settings", function (messageData) {
+    var downloadFile = (function () {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        return function (data, fileName) {
+            var blob = new Blob([data], {type: "octet/stream"}),
+                url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        };
+    }());
+
+
+    var showRawServerLogs = function () {
+        Socket.send("view", {
+            "view": "settings",
+            "action": "logfiles"
+        }, function (messageData) {
+            var el = $('<div>');
+            var files = messageData.files || [];
+            files.sort(function (a, b) {
+                if (new Date(a.time) > new Date(b.time)) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            });
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                el.append(
+                    '<div data-id="' + file.file + '">' +
+                    '<div class="btn btn-info download btn-sm">' +
+                    'Log ' + file.file + ' ' + (file.size / 1024 / 1024).toFixed(3) + 'MB (Last modified: ' + new Date(file.time).toLocaleString() + ')' +
+                    '</div>' +
+                    '</div>'
+                );
+            }
+            el.on("click", ".btn.download", function () {
+                var id = $(this).parent().attr("data-id");
+                Socket.send("view", {
+                    "view": "settings",
+                    "action": "download",
+                    "file": id
+                }, function (messageData) {
+                    downloadFile(messageData.content, id);
+                });
+            });
+            Modal.alert(el);
+        });
+    };
+
     var btn = $(".btn.update");
     btn.on("click", function () {
         Modal.confirm(t("settings.confirm"), function (success) {
-            if(success){
+            if (success) {
                 spinner($(".settings"));
                 Socket.send("view", {
                     "view": "settings",
@@ -14,5 +67,9 @@ View.register("settings", function (messageData) {
                 });
             }
         });
+    });
+    btn = $(".btn.download");
+    btn.on("click", function () {
+        showRawServerLogs();
     });
 });
--- src/views/settings.js
+++ src/views/settings.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var exec = require('child_process').exec;
+var fs = require("fs");
 
 /**
  * The view
@@ -15,14 +16,65 @@ function View(user, messageData, callback) {
         callback({redirect: "index", "note": ["access.denied", "danger"]});
         return;
     }
-    console.log(messageData);
+    var logdir = __dirname + "/../../logs";
     if (messageData.action == "update") {
         exec("cd " + __dirname + "/../.. && git pull", null, function () {
             callback();
         });
         return;
     }
+    if (messageData.action == "logfiles") {
+        fs.stat(logdir, function (err) {
+            if (err) {
+                if (callback) callback();
+                return;
+            }
+            fs.readdir(logdir, function (err, files) {
+                var filesArr = [];
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    if (file.substr(0, 1) == ".") continue;
+                    try {
+                        var stats = fs.statSync(logdir + "/" + file);
+                        filesArr.push({"file": file, "time": stats.mtime, "size": stats.size});
+                    } catch (e) {
+
+                    }
+                }
+                filesArr.sort(function (a, b) {
+                    if (a.file > b.file) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                for (var j = 10; j < filesArr.length; j++) {
+                    fs.unlink(logdir + "/" + filesArr[j].file);
+                    delete filesArr[i];
+                }
+                if (callback) callback({"files" : filesArr});
+            });
+        });
+        return;
+    }
+    if (messageData.action == "download") {
+        var file = logdir + "/" + messageData.file.replace(/\/\\/g, "");
+        fs.stat(file, function (err) {
+            if (err) {
+                callback({"content" : ""});
+                return;
+            }
+            fs.readFile(file, "utf8", function (err, data) {
+                if(data.length > 1024 * 1024){
+                    data = data.substr(-(1024 * 1024 * 0.8));
+                    fs.writeFile(file, data);
+                }
+                callback({"content" : data});
+            });
+        });
+        return;
+    }
     callback();
-};
+}
 
 module.exports = View;
