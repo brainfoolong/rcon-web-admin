a5bb5527eba1d9feeba240ea66a3d2ce0a41ef45
BrainFooLong <roland@bfldev.com>
2017-01-08 15:48:19 +0100

    updated gh pages

---
+++ scripts/global.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(function () {
+    // do some hamburger and navigation magic
+    (function () {
+        var trigger = $('.hamburger'),
+            overlay = $('.overlay'),
+            isClosed = false;
+
+        trigger.click(function () {
+            hamburger_cross();
+        });
+
+        function hamburger_cross() {
+
+            if (isClosed == true) {
+                overlay.hide();
+                trigger.removeClass('is-open');
+                trigger.addClass('is-closed');
+                isClosed = false;
+            } else {
+                overlay.show();
+                trigger.removeClass('is-closed');
+                trigger.addClass('is-open');
+                isClosed = true;
+            }
+        }
+
+        $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').toggleClass('toggled');
+        });
+    })();
+});
